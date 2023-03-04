ae95fc2ac439f92e647f018fea9d028f071b3377
Vbeni <benivillafana@gmail.com>
2023-03-04 11:45:39 -0500

    updated cards dealt

--- java/blackjack.js
+++ java/blackjack.js
@@ -33,6 +33,8 @@ let playerHand =  [];
 let dealerHand = [];
 let undealtCards = shuffleDeck(deck.slice()); //creates shuffled copy of deck to deal cards from 
 
+playerHand.push(...dealCard(undealtCards));
+dealerHand.push(...dealCard(undealtCards));
 playerHand.push(...dealCard(undealtCards)); // spread syntax to fix nested array issue 
 dealerHand.push(...dealCard(undealtCards));
 
@@ -72,37 +74,29 @@ for (let i = 0; i < aces.length; i++){       //loop through aces in hand
 return value;                               //return value of hand
 };
 
-console.log('Player has blackjack: ', hasBlackJack(playerHand));
-console.log('Dealer has blackjack: ', hasBlackJack(dealerHand));
-//28 ways to get blackjack. Hard Code?
-  //if player has blackjack, player wins
-// function hasBlackJack(playerHand){
+console.log('Player hand Value ', hasBlackJack(playerHand));
+console.log('Dealer hand Value: ', hasBlackJack(dealerHand));
 
-// };
-//   //if dealer has blackjack, dealer wins
-// function hasBlackJack(dealerHand){
+  //if neither have blackjack then player turn
+function playerTurn(){        //player chooses to hit or stand 
+                              //if player hits then deal another card
+                              //if player hand > 21 , dealer wins 
+};
 
-// };
-//   //if neither have blackjack then player turn
-// function playerTurn(){        //player chooses to hit or stand 
-//                               //if player hits then deal another card
-//                               //if player hand > 21 , dealer wins 
-// };
+  //if player stands, then dealers turn
+function dealerTurn(){
+                              //if dealers hand > 17, hit until <=17 
+};
 
-//   //if player stands, then dealers turn
-// function dealerTurn(){
-//                               //if dealers hand > 17, hit until <=17 
-// };
+function whoWon(){             //if dealer hand > 21, then player wins 
+                               //if player hand is close to 21 than dealer, player wins
+                               //if dealer hand is closer to 21 then dealer wins
+                               //if hands are = then tie is declared
 
-// function whoWon(){             //if dealer hand > 21, then player wins 
-//                                //if player hand is close to 21 than dealer, player wins
-//                                //if dealer hand is closer to 21 then dealer wins
-//                                //if hands are = then tie is declared
+};
 
-// };
-
-// function reShuffle(){          //PLAY AGAIN
+function reShuffle(){          //PLAY AGAIN
                              
-// };    
+};    
   
-// //MAYBE if time allows, introduce 'HARD' mode 3rd player that hits or stands based on remaining cards in deck 
+//MAYBE if time allows, introduce 'HARD' mode 3rd player that hits or stands based on remaining cards in deck 
